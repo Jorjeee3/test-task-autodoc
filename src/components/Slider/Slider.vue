9fdb00c1fa3df019bac713da936d0465fb381370
Jorjeee3 <look7at7george@gmail.com>
2021-05-11 00:42:41 +0300

    add slider methods

--- src/components/Slider/Slider.vue
+++ src/components/Slider/Slider.vue
@@ -13,10 +13,37 @@ export default {
             currentSlideIndex: 0
         };
     },
-    
+    mounted () {
+        this.startSlider ();
+    },
+
+    methods: {
+        startSlider () {
+            this.timer = setInterval(this.next, 2500);
+        },
+
+        next () {
+            if (this.currentSlideIndex === this.slides.length - 1) {
+                this.currentSlideIndex = 0
+            } else {
+                this.currentSlideIndex += 1
+            }
+        },
+        prev () {
+            this.currentSlideIndex -= 1;
+        }
+    },
+    components: {
+
+    },
+    computed: {
+        slide () {
+            return this.slides[this.currentSlideIndex];
+        }
+    },
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
